8a4c1882cff0c87455323b7582d047e72df42cf1
Cortes <m.cortes@accenture.com>
2023-11-30 10:41:57 -0600

    add introduction component

--- src/components/NavbarMenu.js
+++ src/components/NavbarMenu.js
@@ -4,25 +4,18 @@ import Navbar from 'react-bootstrap/Navbar';
 
 export default function NavbarMenu() {
   return (
-        <Navbar bg="dark" variant="dark">
-            <Container>
-                <Navbar.Brand href="#!">
-                    Dynamic <b>Form</b>
-                </Navbar.Brand>
-                <Navbar.Toggle />
-                <Navbar.Collpase className="justify-contet-end">
-                    <Navbar.Text>
-                        Signed in as: {" "}
-                        <a 
-                            href="https://github.com/MaJu2023" 
+    <Navbar bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#!">Dynamic <b>Form</b> </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a  href="https://github.com/MaJu2023" 
                             target="_blank" 
-                            rel="noreferrer"
-                        >
-                                Judith Cortés
-                        </a>
-                    </Navbar.Text>
-                </Navbar.Collpase >
-            </Container>
-        </Navbar>
+                            rel="noreferrer">Judith Cortés</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
